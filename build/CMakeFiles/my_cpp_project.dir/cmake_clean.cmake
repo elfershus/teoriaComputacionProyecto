b5ebc1d9f0fe4a90f0fc3e7a89da83271e7a9ccc
elfershus <fershus.dev@gmail.com>
2024-11-20 17:44:47 -0600

    first commit

---
+++ build/CMakeFiles/my_cpp_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_cpp_project.dir/src/main.cpp.o"
+  "CMakeFiles/my_cpp_project.dir/src/main.cpp.o.d"
+  "my_cpp_project"
+  "my_cpp_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_cpp_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
